c8d25685c2cfa80b7a1ed5dd5b6ac3368be0a444
Анастасия <a.fevraleva@maii.ru>
2021-08-11 18:23:58 +0200

    the definition consists of a string of letters

--- Strings Methods.py
+++ Strings Methods.py
@@ -7,3 +7,13 @@ print(stringtwwo)
 ## Index Access
 indexstring="Hamsrter"
 print(indexstring[3]+indexstring[1]+indexstring[2])
+
+## Extracting a slice Slice extraction operator: [X:Y].
+# X is the beginning of the slice, and Y is the end;
+# the symbol with the number Y is not included in the slice. By default,
+# the first index is 0, and the second is the length of the string.
+##
+stringdigit="66666"
+print(stringdigit.isdigit())
+notquitestring="668885kkdeeuueur"
+print(notquitestring.isdigit())
