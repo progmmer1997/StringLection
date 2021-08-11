5a6664fad97c34132cf4b9374f8ffde2f909d4df
Анастасия <a.fevraleva@maii.ru>
2021-08-11 17:37:22 +0200

    determining the length of a string

---
+++ Strings Methods.py
@@ -0,0 +1,5 @@
+##determining the length of a string
+# len()
+stringone="hello.world"
+stringtwwo= len(stringone)
+print(stringtwwo)
--- main.py
+++ main.py
@@ -3,3 +3,8 @@ s="Hello"
 ## multiplying a string by a number and printing result
 print(3*s)
 ## Cretion two strings,adding them and printing this
+s1="Hello,"
+s2="Humster!!"
+print(s1+""+s2)
+## adding multiplying result two strings by number and printing
+print(3*s1+" "+2*s2)
